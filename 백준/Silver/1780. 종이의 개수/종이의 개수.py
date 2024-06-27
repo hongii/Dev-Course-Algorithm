f08577735c46f75de94aba0e0b7bd60f1aba0cf2
hongii <93701887+hongii@users.noreply.github.com>
2024-06-27 22:26:11 +0900

    [Silver II] Title: 종이의 개수, Time: 5088 ms, Memory: 69464 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 k = int(input())
 
 paper = []
@@ -7,40 +8,33 @@ for i in range(k):
 	arr = list(map(int, input().split()))
 	paper.append(arr)
 
-# 1) 종이가 모두 같은 수로 되어 있는지 확인
-def isSameAllPapers(arr):
-	numSet = set([num for row in arr for num in row])
-	return len(numSet) == 1
-
 def printPapers(res):
     print(res[-1])
     print(res[0])
     print(res[1])
 
-# 2) 종이를 같은 크기의 종이 9개로 자르고, 각각의 잘린 종이에 대해서 1)의 과정을 반복
-def divideConquer(paper, x1, y1, x2, y2, size):
-    subPapers = [row[y1:y2+1] for row in paper[x1:x2+1]]
-    
-    if size == 1 or isSameAllPapers(subPapers):
-        key = subPapers[0][0]
-        counts[key] += 1
-        return
+def divideConquer(x, y, size):
+    paperNum = paper[x][y]
 
-    new_size = size // 3
-    for i in range(3):
-        for j in range(3):
-            nx1 = x1 + i * new_size
-            ny1 = y1 + j * new_size
-            nx2 = nx1 + new_size - 1
-            ny2 = ny1 + new_size - 1
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if(paper[i][j] != paperNum):
+              new_size = size//3
 
-            divideConquer(paper, nx1, ny1, nx2, ny2, new_size)
+              for ni in range(3):
+                  for nj in range(3):
+                      nx = x + ni * new_size
+                      ny = y + nj * new_size
+                      divideConquer(nx, ny, new_size)
+              return
+            
+    if paperNum == -1:
+        counts[-1] += 1
+    elif paperNum == 0:
+        counts[0] += 1
+    elif paperNum == 1:
+        counts[1] += 1
 
 counts = {-1: 0, 0: 0, 1: 0}
-if isSameAllPapers(paper):
-  key = paper[0][0]
-  counts[key] = 1
-else:
-  divideConquer(paper, 0, 0, k-1, k-1, k)
-
+divideConquer(0, 0, k)
 printPapers(counts)
