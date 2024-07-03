68c40b3f4ca1db9396a7b0410ccc702c30be6780
hongii <93701887+hongii@users.noreply.github.com>
2024-07-03 17:20:42 +0900

    Update N과 M （4）.py

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,4 +1,4 @@
-from itertools import combinations_with_replacement as cbr
+# from itertools import combinations_with_replacement as cbr
 import sys
 input = sys.stdin.readline
 
@@ -6,6 +6,22 @@ n, m = map(int, input().split())
 nums = [i for i in range(1, n+1)]
 
 # sol 1) 중복 조합 라이브러리 사용
-cbrs = cbr(nums, m)
-for cb in cbrs:
-	print(" ".join(map(str, cb)))
+# cbrs = cbr(nums, m)
+# for cb in cbrs:
+# 	print(" ".join(map(str, cb)))
+
+# sol 2) 백트래킹(완전 탐색)
+def combinations(cb, cnt):
+  if cnt == 0:
+    print(" ".join(map(str, cb)))
+    return
+  
+  for i in range(n):
+    if(cb and cb[-1] > nums[i]):
+      continue
+  
+    cb.append(nums[i])
+    combinations(cb, cnt-1)
+    cb.pop()
+
+combinations([], m)
