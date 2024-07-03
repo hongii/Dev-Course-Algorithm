40669ef6f5f5df1549378324eeac949c01b1086a
hongii <93701887+hongii@users.noreply.github.com>
2024-07-03 14:24:41 +0900

    [Silver III] Title: N과 M (3), Time: 1224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -6,6 +6,19 @@ n, m = map(int, input().split())
 nums = [i for i in range(1, n+1)]
 
 # sol 1) 중복 순열 라이브러리 사용
-perm = product(nums, repeat=m)
-for pm in perm:
-	print(" ".join(map(str, pm)))
+# perm = product(nums, repeat=m)
+# for pm in perm:
+# 	print(" ".join(map(str, pm)))
+
+# sol 2) 백트래킹(완전 탐색)
+def permutation(pm, cnt):
+  if cnt == 0:
+    print(" ".join(map(str, pm)))
+    return
+  
+  for i in range(n):
+    pm.append(nums[i])
+    permutation(pm, cnt-1)
+    pm.pop()
+
+permutation([], m)
