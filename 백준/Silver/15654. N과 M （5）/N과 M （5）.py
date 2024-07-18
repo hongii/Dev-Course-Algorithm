78ab7ccad9be125ebf22acdb45d9cf69e24076be
hongii <93701887+hongii@users.noreply.github.com>
2024-07-18 18:02:00 +0900

    Update N과 M （5）.py

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,6 +1,7 @@
 from itertools import permutations as pm
 import sys
-
+import os
+sys.stdin = open(os.getcwd() + "\\input.txt", "rt")
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -11,3 +12,20 @@ pms = list(pm(nums, m))
 pms.sort()
 for p in pms:
   print(" ".join(map(str, p)))
+
+# sol 2) 백트래킹(완전 탐색)
+# def permutation(pm, cnt):
+#   if cnt == 0:
+#     print(" ".join(map(str, pm)))
+#     return
+  
+#   for i in range(n):
+#     if(pm and nums[i] in pm):
+#       continue
+  
+#     pm.append(nums[i])
+#     permutation(pm, cnt-1)
+#     pm.pop()
+
+# nums.sort()
+# permutation([], m)
