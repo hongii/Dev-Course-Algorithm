26085b979dca027893a8838083ab96bcca582f6b
hongii <93701887+hongii@users.noreply.github.com>
2025-03-20 14:21:27 +0900

    [level 2] Title: 지게차와 크레인, Time: 292.22 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -1,49 +1,44 @@
-from itertools import chain
 from collections import deque
 def solution(storage, requests):
     row, col = len(storage), len(storage[0])
     graph = [['0']*(col+2)] + [list("0"+str+"0") for str in storage]+ [['0']*(col+2)]
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    
-    def check_outside(x, y):
+
+    def check_outside(x, y, crane):
+        dx = [-1, 0, 1, 0]
+        dy = [0, 1, 0, -1]
         for i in range(4):
             x_ = x + dx[i]
             y_ = y + dy[i]
             if 0 <= x_ < row + 2 and 0 <= y_ < col + 2:
-                if graph[x_][y_] == '0':
+                if crane:
+                    if graph[x_][y_] == '0' or graph[x_][y_] == graph[x][y]:
+                        return True
+                elif graph[x_][y_] == '0':
                     return True
         return False
     
-    def bfs(target, remove_list):
-        dq = deque()
-        visited = [[False]*(col+2) for _ in range(row+2)]
-        visited[0][0] = True
-        dq.append((0, 0))
-        
-        while dq:
-            x, y = dq.popleft()
-            
-            for i in range(4):
-                x_ = x + dx[i]
-                y_ = y + dy[i]
-                if 0 <= x_ < row + 2 and 0 <= y_ < col + 2 and not visited[x_][y_]:
-                    if  graph[x_][y_] == '0':
-                        visited[x_][y_] = True
-                        dq.append((x_, y_))
-                    elif graph[x_][y_] == target and check_outside(x_, y_):
-                        remove_list.append((x_, y_))
-        for x, y in remove_list:
-            graph[x][y] = '0'
+    def get_updated_value(val, target, check):
+        if val == target:
+            return '0' if check else '1'
+        return val
 
     for str in requests:
-        remove_list = [] 
         if len(str) > 1:
             target = str[0]
-            bfs(target, remove_list)
-            graph = [['0' if char == target else char for char in row] for row in graph]
+            graph = [
+                [get_updated_value(val, target, check_outside(x, y, True)) for y, val in enumerate(row)]
+                for x, row in enumerate(graph)
+            ]
         else:
-            bfs(str, remove_list) 
-        
-    sentence = "".join(char for char in chain(*graph) if char != "0")      
+            remove_list = [] 
+            for x in range(1, row+1):
+                for y in range(1, col+1):
+                    if str == graph[x][y] and check_outside(x, y, False):
+                        remove_list.append((x, y))
+                        
+            while remove_list:
+                a, b = remove_list.pop()
+                graph[a][b] = '0'
+                
+    sentence = "".join(char for char in chain(*graph) if char != "0" and char != "1" )      
     return len(sentence)
