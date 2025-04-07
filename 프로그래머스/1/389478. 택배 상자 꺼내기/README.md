cb65964ea6bc511eb03425049e601bbda562cb62
hongii <93701887+hongii@users.noreply.github.com>
2025-04-07 14:43:43 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.10 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478#) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 0.13 ms
+메모리: 9.13 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 14:40:49
+2025년 04월 07일 14:43:41
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,3 +1,31 @@
+import math
 def solution(n, w, num):
-    answer = 0
-    return answer
+    row_cnt = math.ceil(n/w)
+    cur = 1
+    arr = []
+    for r in range(row_cnt):
+        if r == row_cnt - 1:
+            line = list(range(cur, n+1))
+            line += [0] * (w - len(line))
+        else:
+            line = list(range(cur, cur+w))
+        
+        if r % 2 == 1:
+            line.reverse()
+
+        arr.append(line)
+        cur += w
+
+    for r, row in enumerate(arr):
+        if num in row:
+            c = row.index(num)
+            break
+
+    count = 0
+    for line in reversed(arr):
+        if line[c] == num:
+            break
+        if line[c] != 0:
+            count += 1
+
+    return count + 1
